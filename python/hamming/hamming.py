eea5455a70df834b89a34e8e925c3e2e5299eeb7
Aaron Keck <aaron.keck@kiewit.com>
2018-02-16 10:51:12 -0600

    Python3 tweaks, and updates to the test harnesses require an exception now

--- python/hamming/hamming.py
+++ python/hamming/hamming.py
@@ -1,8 +1,1 @@
-
-def distance(strandA, strandB):
-	test_length = len(strandA) if len(strandA) > len(strandB) else len(strandB)
-	hamming_distance = 0
-	for i in xrange(test_length):
-		if strandA[i] != strandB[i]:
-			hamming_distance += 1
-	return hamming_distance
+def distance(strandA, strandB):    if len(strandA) is not len(strandB):        raise ValueError("Unequal Strand Lengths!")    hamming_distance = 0    for i in range(len(strandA)):        if strandA[i] != strandB[i]:            hamming_distance += 1    return hamming_distance
