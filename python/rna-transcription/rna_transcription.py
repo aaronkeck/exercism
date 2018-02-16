b9d5977f9767eab87734a17cadfd59ac5b2ce8e2
Aaron Keck <aaron.keck@kiewit.com>
2018-02-16 11:40:52 -0600

    rename and reformat to support new tests

--- python/rna-transcription/rna_transcription.py
+++ python/rna-transcription/rna_transcription.py
@@ -1,2 +1,14 @@
-def to_rna(dna_strand):
-    pass
+def to_rna(dna):
+    maps = {
+        'A': 'U',
+        'T': 'A',
+        'G': 'C',
+        'C': 'G'
+    }
+    rna = ''
+    for nucleotide in dna:
+        if nucleotide in maps:
+            rna = rna + maps[nucleotide]
+        else:
+            raise ValueError("Unexpected Character in the input")
+    return rna
