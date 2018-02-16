f17ef00b5a71b47b8c21cb623c8af19697e67b76
Aaron Keck <aaron.keck@kiewit.com>
2018-02-16 10:44:09 -0600

    tweaks to support Python 3 and new line endings

--- python/difference-of-squares/difference_of_squares.py
+++ python/difference-of-squares/difference_of_squares.py
@@ -1,9 +1,1 @@
-
-def square_of_sum(n):
-	return sum([x+1 for x in xrange(n)])**2
-
-def sum_of_squares(n):
-	return sum([(x+1)**2 for x in xrange(n)])
-
-def difference(n):
-	return abs(square_of_sum(n) - sum_of_squares(n))
+def square_of_sum(n):	return sum([x+1 for x in range(n)])**2def sum_of_squares(n):	return sum([(x+1)**2 for x in range(n)])def difference(n):	return abs(square_of_sum(n) - sum_of_squares(n))
