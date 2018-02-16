56f2b0a2a68da788884ed6ffbd4099aa8c6c7cbc
Aaron Keck <aaron.keck@kiewit.com>
2018-02-16 12:08:18 -0600

    rename, reformat, and update to cover a variety of special character tests

--- python/word-count/word_count.py
+++ python/word-count/word_count.py
@@ -1,2 +1,13 @@
+import re
+
 def word_count(phrase):
-    pass
+    ledger = dict()
+
+    for word in re.split('\s\'|\'\s|[,\s.:!&@$%^&\n_]', phrase):
+        key = word.strip().lower()
+        if key:
+            if key in ledger:
+                ledger[key] += 1
+            else:
+                ledger[key] = 1
+    return ledger
